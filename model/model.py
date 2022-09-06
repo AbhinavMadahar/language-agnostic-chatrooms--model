8046df77df9e503ea10974f073665a2acc527ed8
Abhinav Madahar <abhinavmadahar@gmail.com>
2022-09-06 14:18:22 -0400

    Skeleton model

--- model/model.py
+++ model/model.py
@@ -1,3 +1,21 @@
+import torch
+
+
+class EncoderBlock:
+    """
+    A encoder block consisting of a multi-head attention and a positionwise feedforward layer, both wrapped with residual connections and layer norm.
+    """
+
+    def __init__(self) -> None:
+        raise NotImplementedError
+    
+    def init_weights(self) -> None:
+        raise NotImplementedError
+    
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        raise NotImplementedError
+
+
 class Encoder:
     """
     Transformer-based encoder for the machine translation model.
@@ -5,6 +23,26 @@ class Encoder:
 
     def __init__(self):
         raise NotImplementedError
+    
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        raise NotImplementedError
+
+
+class EncoderBlock:
+    """
+    A decoder block consisting of a masked multi-head attention, a multi-body attention using the encoder outputs, and a positionwise feedforward layer,
+    all wrapped with residual connections and layer norm.
+    """
+
+    def __init__(self) -> None:
+        raise NotImplementedError
+    
+    def init_weights(self) -> None:
+        raise NotImplementedError
+    
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        raise NotImplementedError
+
 
 class Decoder:
     """
@@ -12,4 +50,10 @@ class Decoder:
     """
 
     def __init__(self):
+        raise NotImplementedError
+
+    def init_state(self) -> None:
+        raise NotImplementedError
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         raise NotImplementedError
