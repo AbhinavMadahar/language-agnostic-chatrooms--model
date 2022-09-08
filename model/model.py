a66af444bdf41f548a115865de8bc23660b91f56
Abhinav Madahar <abhinavmadahar@gmail.com>
2022-09-07 20:12:51 -0500

    Implement decoder class

--- model/model.py
+++ model/model.py
@@ -1,67 +1,8 @@
 import math
 import torch
 
-from collections import Iterable, OrderedDict
+from collections import Iterable, List, OrderedDict, Tuple
 from torch import nn
-
-
-# we implement the main Encoder and Decoder classes at the top.
-# these are the only classes that should be used outside this module.
-
-class Encoder(nn.Module):
-    """
-    Transformer-based encoder for the machine translation model.
-    """
-
-    def __init__(self,
-                 vocab_size: int, 
-                 num_hiddens: int, 
-                 ffn_num_hiddens: int,
-                 num_heads: int,
-                 num_blocks: int,
-                 dropout: float,
-                 max_length: int,
-                 use_bias: bool = False
-        ) -> None:
-        super().__init__()
-
-        self.num_hiddens = num_hiddens
-        self.attention_weights = []
-
-        self.embedding = nn.Embedding(vocab_size, num_hiddens)
-        self.position_encoding = PositionEncoding(num_hiddens, dropout, max_length)
-        self.blocks = nn.Sequential(OrderedDict([
-            (f'block {i}', EncoderBlock(num_hiddens, ffn_num_hiddens, num_heads, dropout, use_bias)) for i in range(num_blocks)
-        ]))
-    
-    def forward(self, x: torch.Tensor, valid_lengths: Iterable[int]) -> torch.Tensor:
-        # don't worry about why we multiply by sqrt(num_hiddens).
-        # it's a long story.
-        # maybe I'll one day update this comment to explain why.
-        x = self.position_encoding(self.embedding(x) * math.sqrt(self.num_hiddens))
-
-        self.attention = []
-        # we go pass x through each block
-        for block in self.blocks:
-            x = block(x, valid_lengths)
-            self.attention_weights.append(block.attention.attention.attention_weights)
-        
-        return x
-
-
-class Decoder(nn.Module):
-    """
-    Transformer-based decoder for the machine translation model.
-    """
-
-    def __init__(self):
-        raise NotImplementedError
-
-    def init_state(self) -> None:
-        raise NotImplementedError
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        raise NotImplementedError
 
 
 # the following classes are helpers for the Encoder and Decoder classes.
@@ -85,7 +26,7 @@ class DecoderBlock(nn.Module):
     all wrapped with residual connections and layer norm.
     """
 
-    def __init__(self) -> None:
+    def __init__(self, num_hiddens: int, ffn_num_hiddens: int, num_heads: int, dropout: float, i: int) -> None:
         raise NotImplementedError
     
     def init_weights(self) -> None:
@@ -105,3 +46,90 @@ class PositionEncoding(nn.Module):
     
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         raise NotImplementedError
+
+
+# we implement the main Encoder and Decoder classes now.
+# these are the only classes that should be used outside this module.
+
+class Encoder(nn.Module):
+    """
+    Transformer-based encoder for the machine translation model.
+    """
+
+    def __init__(self,
+                 vocab_size: int, 
+                 num_hiddens: int, 
+                 ffn_num_hiddens: int,
+                 num_heads: int,
+                 num_blocks: int,
+                 dropout: float,
+                 max_length: int,
+                 use_bias: bool = False
+        ) -> None:
+        super().__init__()
+
+        self.num_hiddens = num_hiddens
+        self.attention_weights = None
+
+        self.embedding = nn.Embedding(vocab_size, num_hiddens)
+        self.position_encoding = PositionEncoding(num_hiddens, dropout, max_length)
+        self.blocks = nn.Sequential(OrderedDict(
+            (f'block {i}', EncoderBlock(num_hiddens, ffn_num_hiddens, num_heads, dropout, use_bias)) for i in range(num_blocks)
+        ))
+    
+    def forward(self, x: torch.Tensor, valid_lengths: Iterable[int]) -> torch.Tensor:
+        # don't worry about why we multiply by sqrt(num_hiddens).
+        # it's a long story.
+        # maybe I'll one day update this comment to explain why.
+        x = self.position_encoding(self.embedding(x) * math.sqrt(self.num_hiddens))
+
+        self.attention = []
+        # we go pass x through each block
+        for block in self.blocks:
+            x = block(x, valid_lengths)
+            self.attention_weights.append(block.attention.attention.attention_weights)
+        
+        return x
+
+
+class Decoder(nn.Module):
+    """
+    Transformer-based decoder for the machine translation model.
+    """
+
+    def __init__(self, vocab_size: int, num_hiddens: int, ffn_num_hiddens: int, num_heads: int, num_blocks: int, dropout: float, max_length: int) -> None:
+        super().__init__()
+
+        self.num_hiddens = num_hiddens
+        self.num_blocks = num_blocks
+
+        self.embedding = nn.Embedding(vocab_size, num_hiddens)
+        self.position_encoding = PositionEncoding(num_hiddens, dropout, max_length)
+
+        self.blocks = nn.Sequential(OrderedDict(
+            (f'block {i}', DecoderBlock(num_hiddens, ffn_num_hiddens, num_heads, dropout, i)) for i in range(num_blocks)
+        ))
+        self.dense = nn.LazyLinear(vocab_size)
+
+        self._attention_weights = None
+
+    def init_state(self, encoder_outputs: List[torch.Tensor], encoder_valid_lengths: List[int]) -> None:
+        return [encoder_outputs, encoder_valid_lengths, [None] * self.num_blocks]
+
+    def forward(self, x: torch.Tensor, state: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+        x = self.position_encoding(self.embedding(x) * math.sqrt(self.num_hiddens))
+
+        # the first list is the decoder self-attention weights, and the second list is the encoder-decoder attention weights
+        self._attention_weights = [[], []]
+        for i, block in enumerate(self.blocks):
+            x, state = block(x, state)
+            self._attention_weights[0].append(block.attention1.attention.attention_weights)
+            self._attention_weights[1].append(block.attention2.attention.attention_weights)
+
+        x = self.dense(x)
+        
+        return x, state
+    
+    @property
+    def attention_weights(self) -> Tuple[List[torch.Tensor], List[torch.Tensor]]:
+        return self._attention_weights
